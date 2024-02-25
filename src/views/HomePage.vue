178f0b572449cba8f08c4fbbf5ada9146edb9b0a
LSD-hash <1216395765@qq.com>
2024-02-25 18:33:03 +0800

    增加home和usr页面

---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>这里是home</h1>
+</template>
+<script>
+export default ({
+    data() {
+        return {}
+    },
+})
+</script>
---
+++ src/views/UsrPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>这里是usr</h1>
+</template>
+<script>
+export default ({
+    data() {
+        return {}
+    },
+})
+</script>
